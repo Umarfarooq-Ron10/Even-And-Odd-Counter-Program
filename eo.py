a4ac31b696d8e156ace64ae94813fa94a5732aff
Umarfarooq-Ron10 <umarron.10@gmail.com>
2025-10-06 16:43:44 +0530

    Save this to prime-count

--- eo.py
+++ eo.py
@@ -1,17 +1,27 @@
 
-numbers = input("Enter numbers separated by spaces: ").split()
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
+numbers = input("Enter numbers separated by spaces: ").split()
 numbers = [int(num) for num in numbers]
 
 even_count = 0
 odd_count = 0
+prime_count = 0
 
 for num in numbers:
     if num % 2 == 0:
         even_count += 1
     else:
         odd_count += 1
-
+    if is_prime(num):
+        prime_count += 1
 
 print(f"\nTotal Even Numbers: {even_count}")
 print(f"Total Odd Numbers: {odd_count}")
+print(f"Total Prime Numbers: {prime_count}")
